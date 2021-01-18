673d86621608364079767f006f91ac90a830b0a3
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:14:11 -0300

    Adicionar arquivos de projeto.

---
+++ MembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Entre o valor do raio: ");
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Calculadora.Circuferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor do PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
